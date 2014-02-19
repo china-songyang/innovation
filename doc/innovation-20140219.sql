de431831dc350e1f510bb03e2e7498c40fd995fc
home <home@HOME>
2014-02-19 23:32:00 +0800

    数据库脚本-songyang

---
+++ doc/innovation-20140219.sql
@@ -0,0 +1,227 @@
+﻿/*
+SQLyog Enterprise - MySQL GUI v6.0
+Host - 5.0.96-community-nt : Database - innovation
+*********************************************************************
+Server version : 5.0.96-community-nt
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+create database if not exists `innovation`;
+
+USE `innovation`;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+/*Table structure for table `app_dict` */
+
+DROP TABLE IF EXISTS `app_dict`;
+
+CREATE TABLE `app_dict` (
+  `id` int(11) NOT NULL auto_increment,
+  `key` varchar(100) default NULL,
+  `value` varchar(100) default NULL,
+  `type` varchar(100) default NULL,
+  `pinyin` varchar(100) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8;
+
+/*Data for the table `app_dict` */
+
+insert  into `app_dict`(`id`,`key`,`value`,`type`,`pinyin`) values (1,'article.state.normal','普通','article.state','putong'),(2,'article.state.recommand','推荐','article.state','tuijian'),(3,'article.state.top','置顶','article.state','zhiding'),(4,'ascii','ACSII码','regex','acsii'),(10,'boolean.false','否','boolean','fou'),(11,'boolean.true','是','boolean','shi'),(20,'chinese','中文','regex','zhengwen'),(24,'content.mode.more','多项','content.mode','duoxiang'),(25,'content.mode.none','空项','content.mode','kongxiang'),(26,'content.mode.one','单项','content.mode','danxiang'),(28,'decmal','浮点数','regex','fudianshu'),(29,'email','邮箱','regex','youxiang'),(39,'id','标识','regex','biaoshi'),(40,'idcard','身份证','regex','shenfenzheng'),(41,'integer','整数','regex','zhengshu'),(42,'letter','字母','regex','zimu'),(43,'loginId','登录标识','regex','denglubiaoshi'),(46,'node.condition.auth','权限','node.condition.model','qanxian'),(47,'node.condition.auth.department','组织机构','node.condition.auth','bumen'),(48,'node.condition.auth.role','角色','node.condition.auth','juese'),(49,'node.condition.auth.user','用户','node.condition.auth','yonghu'),(53,'notempty','非空','regex','feikong'),(54,'number','数字','regex','shuzi'),(55,'phone','手机电话','regex','shouji'),(56,'picture','图片','regex','tupian'),(57,'qq','QQ号','regex','qq'),(58,'rar','压缩文件','regex','yasuowenjian'),(59,'tel','座机电话','regex','zuoji'),(60,'url','URL','regex','url'),(61,'visible.false','隐藏','visible','yinchang'),(62,'visible.true','显示','visible','xianshi'),(63,'zipcode','邮编','regex','youbian'),(64,'只读','只读','node.state','zhidu'),(65,'必填','必填','node.state','bitian'),(66,'选填','选填','node.state','xuantian'),(67,'隐藏','隐藏','node.state','yincang'),(95,'女','女','gender','nv'),(96,'男','男','gender','nan'),(97,'是','是','easyboolean','shi'),(98,'否','否','easyboolean','fou'),(99,'message.state.false','未回复','message.state','weihuifu'),(100,'message.state.true','已回复','message.state','yihuifu');
+
+/*Table structure for table `app_resources` */
+
+DROP TABLE IF EXISTS `app_resources`;
+
+CREATE TABLE `app_resources` (
+  `id` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `path` varchar(100) default NULL,
+  `parentId` varchar(100) default NULL,
+  `sort` int(11) NOT NULL auto_increment,
+  PRIMARY KEY  (`sort`)
+) ENGINE=InnoDB AUTO_INCREMENT=110 DEFAULT CHARSET=utf8;
+
+/*Data for the table `app_resources` */
+
+insert  into `app_resources`(`id`,`name`,`path`,`parentId`,`sort`) values ('auth','管理权限',NULL,NULL,1),('auth.user','管理用户','/app/auth/user/showUsers.jsp','auth',2),('auth.role','管理角色','/app/auth/role/showRoles.jsp','auth',3),('auth.resource','管理资源','/app/auth/resource/showResources.jsp','auth',4),('configuration','管理基础配置','',NULL,6),('configuration.dict','管理数据字典','/app/system/dict/showDicts.jsp','configuration',7),('website','管理网站','',NULL,8),('website.content','管理目录','/app/website/content/show.jsp','website',14),('website.article','管理文章','/app/website/article/show.jsp','website',32),('auth.change.password','修改密码','/app/auth/user/changePassword.jsp',NULL,57),('website.message','管理留言','/app/website/message/show.jsp','website',103),('website.jlb','管理文章','/app/website/admin/main.jsp?cid=innovation','website',104),('website.doorhtml','管理代码','/app/website/admin/main.jsp?cid=doorhtml','website',106),('website.image','管理图片','/app/website/admin/main.jsp?cid=doorimage','website',109);
+
+/*Table structure for table `auth_re_role_resource` */
+
+DROP TABLE IF EXISTS `auth_re_role_resource`;
+
+CREATE TABLE `auth_re_role_resource` (
+  `roleId` int(100) NOT NULL,
+  `resourceId` varchar(100) character set gb2312 NOT NULL default '',
+  PRIMARY KEY  (`roleId`,`resourceId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `auth_re_role_resource` */
+
+insert  into `auth_re_role_resource`(`roleId`,`resourceId`) values (1,'auth.change.password'),(1,'website'),(1,'website.content'),(1,'website.geneon'),(1,'website.geneonproduct'),(1,'website.message'),(5,'auth'),(5,'auth.change.password'),(5,'auth.resource'),(5,'auth.role'),(5,'auth.user'),(5,'configuration'),(5,'configuration.dict'),(5,'website'),(5,'website.content'),(5,'website.geneon'),(5,'website.geneonhtml'),(5,'website.geneonproduct'),(5,'website.message');
+
+/*Table structure for table `auth_re_user_resource` */
+
+DROP TABLE IF EXISTS `auth_re_user_resource`;
+
+CREATE TABLE `auth_re_user_resource` (
+  `userId` varchar(100) NOT NULL,
+  `resourceId` varchar(100) NOT NULL,
+  PRIMARY KEY  (`userId`,`resourceId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `auth_re_user_resource` */
+
+insert  into `auth_re_user_resource`(`userId`,`resourceId`) values ('admin','website.article'),('system','auth'),('system','auth.change.password'),('system','auth.resource'),('system','auth.role'),('system','auth.user'),('system','configuration'),('system','configuration.dict'),('system','website'),('system','website.content'),('system','website.jlb');
+
+/*Table structure for table `auth_re_user_role` */
+
+DROP TABLE IF EXISTS `auth_re_user_role`;
+
+CREATE TABLE `auth_re_user_role` (
+  `userId` varchar(100) character set gb2312 NOT NULL,
+  `roleId` int(100) NOT NULL,
+  PRIMARY KEY  (`userId`,`roleId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `auth_re_user_role` */
+
+insert  into `auth_re_user_role`(`userId`,`roleId`) values ('admin',1),('system',5);
+
+/*Table structure for table `auth_roles` */
+
+DROP TABLE IF EXISTS `auth_roles`;
+
+CREATE TABLE `auth_roles` (
+  `id` int(100) NOT NULL auto_increment,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(500) default NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+/*Data for the table `auth_roles` */
+
+insert  into `auth_roles`(`id`,`name`,`description`) values (1,'管理员','拥有网站维护权限'),(5,'系统维护员','开发人员专用');
+
+/*Table structure for table `auth_users` */
+
+DROP TABLE IF EXISTS `auth_users`;
+
+CREATE TABLE `auth_users` (
+  `Id` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `email` varchar(100) default NULL,
+  `phone` varchar(100) default NULL,
+  PRIMARY KEY  (`Id`),
+  UNIQUE KEY `id` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `auth_users` */
+
+insert  into `auth_users`(`Id`,`name`,`password`,`email`,`phone`) values ('admin','管理员','lueSGJZetyySpUndWjMBEg==','',''),('system','开发维护员','lueSGJZetyySpUndWjMBEg==','','');
+
+/*Table structure for table `web_article` */
+
+DROP TABLE IF EXISTS `web_article`;
+
+CREATE TABLE `web_article` (
+  `id` int(100) NOT NULL auto_increment,
+  `cid` varchar(100) default NULL,
+  `title` varchar(200) default NULL,
+  `text` varchar(20000) default NULL,
+  `type` varchar(100) default NULL,
+  `typeName` varchar(100) default NULL,
+  `state` varchar(100) default NULL,
+  `stateName` varchar(100) default NULL,
+  `count` int(11) default '0',
+  `createTime` datetime default NULL,
+  `operater` varchar(100) default NULL,
+  `operaterName` varchar(100) default NULL,
+  `image` varchar(500) default NULL,
+  `sort` int(11) default '0',
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `web_article` */
+
+/*Table structure for table `web_content` */
+
+DROP TABLE IF EXISTS `web_content`;
+
+CREATE TABLE `web_content` (
+  `id` varchar(100) NOT NULL,
+  `name` varchar(100) default NULL,
+  `english` varchar(100) default NULL,
+  `path` varchar(200) default NULL,
+  `parentId` varchar(100) default NULL,
+  `sort` int(11) NOT NULL auto_increment,
+  `hasShow` varchar(100) default NULL,
+  `hasShowName` varchar(100) default NULL,
+  `mode` varchar(100) default 'content.mode.one',
+  `modeName` varchar(100) default '单一',
+  `text` text,
+  PRIMARY KEY  (`sort`)
+) ENGINE=InnoDB AUTO_INCREMENT=501 DEFAULT CHARSET=utf8;
+
+/*Data for the table `web_content` */
+
+insert  into `web_content`(`id`,`name`,`english`,`path`,`parentId`,`sort`,`hasShow`,`hasShowName`,`mode`,`modeName`,`text`) values ('innovation','创新创业平台','innovation','',NULL,463,'visible.true','显示','content.mode.none','空项',NULL),('innovation.hot','最新热点','redian','','innovation',496,'visible.true','显示','content.mode.more','多项',''),('innovation.practice','创新实践','shijian','','innovation',497,'visible.true','显示','content.mode.more','多项',''),('innovation.harvest','重点成果','chengguo','','innovation',498,'visible.true','显示','content.mode.more','多项',''),('innovation.education','创新教育','jiaoyu','','innovation',499,'visible.true','显示','content.mode.more','多项',''),('innovation.notice','通知通告','tonggao','','innovation',500,'visible.true','显示','content.mode.more','多项','');
+
+/*Table structure for table `web_message` */
+
+DROP TABLE IF EXISTS `web_message`;
+
+CREATE TABLE `web_message` (
+  `id` int(11) NOT NULL auto_increment,
+  `cid` varchar(100) default NULL,
+  `state` varchar(100) default NULL,
+  `stateName` varchar(100) default NULL,
+  `hasShow` varchar(100) default NULL,
+  `hasShowName` varchar(100) default NULL,
+  `sendTime` datetime default NULL,
+  `sendText` varchar(1000) default NULL,
+  `sendEmail` varchar(100) default NULL,
+  `sendPhone` varchar(100) default NULL,
+  `sendName` varchar(100) default NULL,
+  `receiveTime` varchar(100) default NULL,
+  `receiveText` varchar(1000) default NULL,
+  `receiveOperater` varchar(100) default NULL,
+  `receiveOperaterName` varchar(100) default NULL,
+  `type` varchar(50) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `web_message` */
+
+/*Table structure for table `web_recommend` */
+
+DROP TABLE IF EXISTS `web_recommend`;
+
+CREATE TABLE `web_recommend` (
+  `id` int(10) NOT NULL auto_increment,
+  `brand` varchar(100) default NULL,
+  `model` varchar(100) default NULL,
+  `oldCost` varchar(100) default NULL,
+  `nowCost` varchar(100) default NULL,
+  `createtime` varchar(100) default NULL,
+  `factory` varchar(100) default NULL,
+  `type` varchar(100) default NULL,
+  `typename` varchar(100) default NULL,
+  `state` varchar(100) default NULL,
+  `statename` varchar(100) default NULL,
+  `text` varchar(5000) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `web_recommend` */
+
+insert  into `web_recommend`(`id`,`brand`,`model`,`oldCost`,`nowCost`,`createtime`,`factory`,`type`,`typename`,`state`,`statename`,`text`) values (3,'惠邦木业','HB-4456-4暖白','1990','1888','2013-07-11 22:05:17','惠邦木业有限公司','article.state.normal','普通','visible.true','显示','sasdasdasdqwd');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
